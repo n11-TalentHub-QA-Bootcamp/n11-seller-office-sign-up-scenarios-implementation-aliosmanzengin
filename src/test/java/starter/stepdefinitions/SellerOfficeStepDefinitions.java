ad4367beb765ba1a8f650af520104a3fa5a5320c
Ali Osman Zengin <66180039+aliosmanzengin@users.noreply.github.com>
2021-12-19 00:51:44 +0300

    homework ali o zengin

---
+++ src/test/java/starter/stepdefinitions/SellerOfficeStepDefinitions.java
@@ -0,0 +1,66 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.*;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Click;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+import starter.selleroffice.navigation.loginAndSignup.Login;
+import starter.selleroffice.navigation.loginAndSignup.SignUp;
+import starter.selleroffice.navigation.pages.sellerOfficeLoginPage;
+import starter.selleroffice.navigation.tasks.NavigateTo;
+
+
+public class SellerOfficeStepDefinitions {
+
+    Actor actor = Actor.named("patikabootcampers");
+
+    @Managed
+    WebDriver webDriver;
+
+    @Given("user launch browser and open the login page")
+    public void user_launch_browser_and_open_the_login_page() {
+        actor.can(BrowseTheWeb.with(webDriver));
+        actor.wasAbleTo(NavigateTo.sellerOfficeLoginPage());
+ //       actor.attemptsTo(Open.url("https://so.n11.com/"));
+    }
+
+    @When("user signed in selleroffice with valid credentials")
+    public void user_signed_in_selleroffice_with_valid_credentials() {
+        actor.attemptsTo(Login.asNewUser("valid user name","valid password"));
+    }
+
+    @Then("user signed in selleroffice successfully")
+    public void user_signed_in_selleroffice_successfully() {
+        // to be continued
+    }
+
+    @When("user navigates to sign up page")
+    public void userNavigatesToSignUpPage() {
+        actor.attemptsTo(
+                Click.on(sellerOfficeLoginPage.SIGN_UP_BTN)
+        );
+    }
+
+    @And("user fills the required fields but not accept the aggreement")
+    public void userFillsTheRequiredFieldsButNotAcceptTheAggreement() {
+        actor.attemptsTo(
+                SignUp.signUpTheSystem("alican","aliCAN123.","alican@amail.com","alicanbakkal")
+        );
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Then("verify user see an error message displayed")
+    public void verifyUserSeeAnErrorMessageDisplayed() {
+
+    }
+
+
+}
